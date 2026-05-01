44b13d387d63a81967965025f5eb39f1634d6dee
VITO HACKER <vitohackergrgic@gmail.com>
2026-05-01 18:18:16 +0200

    build: update Firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnEtaXTVFsZBtwY4mbtkMVlFgeYxxEp6o",
-  authDomain: "arcadia-1ea2a.firebaseapp.com",
-  projectId: "arcadia-1ea2a",
-  storageBucket: "arcadia-1ea2a.firebasestorage.app",
-  messagingSenderId: "939838520725",
-  appId: "1:939838520725:web:ae31b1efca715fbf5e8a9c",
-  measurementId: "G-3QPJ8PFKXG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
