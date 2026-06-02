e14b38f3523c40ea5c5b376c51da3d1b5f1b5bf5
Vito Hacker <vitohacker@smartwifi-be99.home>
2026-06-02 18:09:37 +0200

    feat: add sitemap and robots config

---
+++ app/robots.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/dashboard',
+        '/insights',
+        '/focus',
+        '/settings',
+        '/auth',
+      ],
+    },
+    sitemap: 'https://app-arcadia.vercel.app',
+  }
+}
